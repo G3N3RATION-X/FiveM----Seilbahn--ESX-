ca456ac1d6ef5167634b29215e3ea7cecf45070f
®️Bylickilabs™️ <102898784+G3N3RATION-X@users.noreply.github.com>
2023-11-15 17:32:08 +0100

    FiveM </ Seilbahn
    Co-Authored-By: ®️BYLICKILABS™️ <109308073+bylickilabs@users.noreply.github.com>

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+
+description "®©BYLICKILABS </ FiveM - Seilbahn"
+
+author 		'Thorsten Bylicki'
+name 		'BYLICKILABS'
+version 	'v1'
+
+
+client_script "cablecar.lua"
+server_script "cablecar_controller.lua"
